3968be55b74cb6c0d849b8751ccad0abe97bc882
jjlr <mc.jjlr@tutanota.com>
2019-09-11 00:52:10 -0700

    Added raw apple pie, and wheat dough. Added logger. Created english lang file.

--- src/main/java/jjlr/culinaryadvancement/Main.java
+++ src/main/java/jjlr/culinaryadvancement/Main.java
@@ -1,11 +1,18 @@
 package jjlr.culinaryadvancement;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.loader.api.Version;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import jjlr.culinaryadvancement.items.*;
 
 public class Main implements ModInitializer {
 
-	public static String MODID = "culinaryadvancement";
+	public static final String MODID = "culinaryadvancement";
+	public static final String VERSION = "0.1.0";
+	public static final Logger logger = LogManager.getLogger("culinaryadvancementlogger");
 
 	@Override
 	public void onInitialize() {
@@ -13,8 +20,9 @@ public class Main implements ModInitializer {
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
 
-		System.out.println("Hello Fabric world!");
+		logger.info("Culinary advancement version %s started with modid %s .", VERSION, MODID);
 
 		IngredientItems.registerItems();
+		CraftedFood.registerItems();
 	}
 }
--- src/main/java/jjlr/culinaryadvancement/items/CraftedFood.java
+++ src/main/java/jjlr/culinaryadvancement/items/CraftedFood.java
@@ -1,5 +1,19 @@
 package jjlr.culinaryadvancement.items;
 
+import net.minecraft.item.FoodComponent;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
+
 public class CraftedFood{
     
+    public static final Item APPLE_PIE_UNCOOKED = new Item(new Item.Settings().group(ItemGroup.FOOD).food(new FoodComponent.Builder().hunger(2).saturationModifier(0.2f).build()));
+    public static final Item WHEAT_DOUGH = new Item(new Item.Settings().group(ItemGroup.FOOD).food(new FoodComponent.Builder().hunger(2).saturationModifier(0.1f).build()));
+
+    public static void registerItems(){
+
+        Registry.register(Registry.ITEM, new Identifier("apple_pie_uncooked"), APPLE_PIE_UNCOOKED);
+        Registry.register(Registry.ITEM, new Identifier("wheat_dough"), WHEAT_DOUGH);
+    }
 }
